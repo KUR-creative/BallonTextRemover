2cafd3dbe9a4d3d839ca60fd0386bbd512e8a650
kur <rhdnfka94@gmail.com>
2018-07-10 15:52:24 +0900

    Genralize origin image path input
    이전에는 원본 이미지 디렉토리가 반드시 레포지토리 내에 존재하여야 했다.
    이를 일반화하여 이미지 디렉토리의 위치나 포함관계에 상관없이 사용할 수 있게 하였다.
    
    Previously, the original image directory had to exist in the repository.
    Now, we generalized it so that it can be used irrespective of the location
    or inclusion relation of the image directory.

--- balloonTextMaskerMain.py
+++ balloonTextMaskerMain.py
@@ -14,19 +14,23 @@ import cv2
 import numpy as np
 import bubbleFinder  # github.com/sKHJ/speechBubbleFinder
 import ballTextMasker
+from pathlib import Path
 import utils
 
 
 def main(origin_dir, cleaned_dir, mask_dir):
-    utils.safe_mkdir(cleaned_dir)
-    utils.safe_mkdir(mask_dir)
+    utils.safe_copytree(origin_dir, cleaned_dir, ['*.jpg', '*.jpeg', '*.png'])
+    utils.safe_copytree(origin_dir, mask_dir, ['*.jpg', '*.jpeg', '*.png'])
     textFinder = ballTextMasker.BalloonCleaner()
 
     for origin_path in utils.file_paths(origin_dir):
-        cleaned_path = 
-        mask_path = 'mask\\' + fileName.split('\\')[-1]
+        old_parent = Path(origin_dir).parts[-1]
+        cleaned_path = utils.make_dstpath(origin_path, old_parent, cleaned_dir) 
+        mask_path  = utils.make_dstpath(origin_path, old_parent, mask_dir) 
         print(origin_path,cleaned_path,mask_path)
         img = cv2.imread(origin_path)
+        cv2.imshow('img',img); cv2.waitKey(0)
+        '''
         mask = np.zeros(img.shape,np.uint8)
         data = bubbleFinder.bubbleFinder(img)
         for [x, y, w, h] in data:
@@ -37,6 +41,7 @@ def main(origin_dir, cleaned_dir, mask_dir):
             #cv2.waitKey(0)
         cv2.imwrite(cleaned_path,img)
         cv2.imwrite(mask_path, mask)
+        '''
 
 if __name__ == '__main__':
     main(sys.argv[1], 'cleaned', 'mask')
